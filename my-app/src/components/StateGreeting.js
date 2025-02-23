17662b5351f50c5ff131a28df74ae316ce9a1b7e
BekMart <bekmart1989@gmail.com>
2025-02-23 15:35:46 +0000

    Add constructor to functionality
    Use super(props) and this.state to create dynamic functionality

--- my-app/src/components/StateGreeting.js
+++ my-app/src/components/StateGreeting.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-class StateGreeting extends React.Component{
-    render() {
-        return <h1>Hello {this.props.name}! {this.props.greeting} </h1>
-    }
+class StateGreeting extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      introduction: "Hello!",
+      buttonText: "Exit",
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.introduction} {this.props.greeting}</h1>
+        <button>{this.state.buttonText}</button>
+      </div>
+    );
+  }
 }
 
 export default StateGreeting;
