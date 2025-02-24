c9968e9ce3cb10ca6bfd790a2266ffe819abde6b
BekMart <bekmart1989@gmail.com>
2025-02-24 11:45:27 +0000

    Create file with ES7Snippet codes for reference

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,51 @@
+// ///rfce creates a functional component with an export statement
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+// ///rafce creates an arrow function with an export statement
+
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+// // rce creates a class component with an export statement
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+// /// rconst adds a default constructor for a class that contains props as arguments
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+// // clg to print an object to the console
+// console.log(first)
+
+// // clo to log an object along with its name as a string
+// console.log('first', first)
+
