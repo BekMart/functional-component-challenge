a40d72c736b926c5cd12897bb2d9c96072f7f5d0
BekMart <bekmart1989@gmail.com>
2025-02-24 12:33:04 +0000

    Create Event handlers for buttons
    Example of functional and class components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,16 @@
-import css from './App.module.css';
-import NavBarSimple from './components/NavBarSimple';
-import Sidebar from './components/Sidebar';
+// import css from './App.module.css';
+// import NavBarSimple from './components/NavBarSimple';
+// import Sidebar from './components/Sidebar';
+import EventsClass from './components/EventsClass';
+import EventsFunctional from './components/EventsFunctional';
 
 function App() {
   return (
-    <div className={css.App}>
-      <NavBarSimple name="Becki" />
-      <Sidebar />
+    <div>
+      {/* <NavBarSimple name="Becki" />
+      <Sidebar /> */}
+      <EventsFunctional />
+      <EventsClass />
     </div>
   );
 }
