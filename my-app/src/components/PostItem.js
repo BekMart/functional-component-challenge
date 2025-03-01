aff473e2d5765c58718176f49717bd4f4ada46fd
BekMart <bekmart1989@gmail.com>
2025-03-01 12:51:24 +0000

    Simplify child element component

--- my-app/src/components/PostItem.js
+++ my-app/src/components/PostItem.js
@@ -4,12 +4,12 @@ import CSS from "./css/PostItem.module.css";
 function PostItem(props) {
     return (
         props.savedPosts.map(post => {
-            const posts = props.posts
-                return <div className={CSS.SearchItem} key={post.title}>
-                    <p>{post.title}</p>
-                    <p>{post.name}</p>
-                    <img src={post.image} alt=""/>
-                    <p>{post.description}</p>                       
+            const { name, title, description, image } = post;
+                return <div className={CSS.SearchItem} key={title}>
+                    <p>{title}</p>
+                    <p>{name}</p>
+                    <img src={image} alt=""/>
+                    <p>{description}</p>                       
                 </div>
             }
         )
