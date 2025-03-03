33673352a133f9ad47f3d660a19b6dbf493aa718
BekMart <bekmart1989@gmail.com>
2025-03-03 15:14:03 +0000

    Use useEffect hook to disable interval function
    recreate componentWillUnmount function for stateless components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,9 @@
 import "./App.module.css";
-import UseEffectCounter from "./components/UseEffectCounter";
+import UseEffectCounterContainer from "./components/UseEffectCounterContainer";
 
 function App() {
   return <div className="App">
-      <UseEffectCounter />
+      <UseEffectCounterContainer />
   </div>;
 }
 
